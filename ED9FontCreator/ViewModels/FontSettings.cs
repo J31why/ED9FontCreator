c2c85b1e30f1012b879d43feb7489f41fc82cb21
J31why <ODHuIy999@outlook.com>
2024-09-20 21:46:14 +0800

    字符宽度完全修正

--- ED9FontCreator/ViewModels/FontSettings.cs
+++ ED9FontCreator/ViewModels/FontSettings.cs
@@ -8,9 +8,9 @@ namespace ED9FontCreator.ViewModels
         [ObservableProperty] private short _fontSize = 41;
         [ObservableProperty] private string _fontWeight = nameof(Avalonia.Media.FontWeight.Bold);
         [ObservableProperty] private string _fontStyle = nameof(Avalonia.Media.FontStyle.Normal);
-        [ObservableProperty] private short _fontMarginL = 1;
-        [ObservableProperty] private short _fontMarginT = -8;
-        [ObservableProperty] private short _fontMarginR = 1;
+        [ObservableProperty] private short _fontMarginL = 0;
+        [ObservableProperty] private short _fontMarginT = -4;
+        [ObservableProperty] private short _fontMarginR = 0;
         [ObservableProperty] private short _fontMarginB = 0;
 
     }
