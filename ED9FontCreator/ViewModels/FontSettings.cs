1fe71cbbc263e03619f0403b9b98b099c132a708
J31why <ODHuIy999@outlook.com>
2024-09-24 11:14:01 +0800

    Merge

--- ED9FontCreator/ViewModels/FontSettings.cs
+++ ED9FontCreator/ViewModels/FontSettings.cs
@@ -4,8 +4,8 @@ namespace ED9FontCreator.ViewModels
 {
     public partial class FontSettings : ViewModelBase
     {
-        [ObservableProperty] private string _fontName = "Source Han Serif CN";
-        [ObservableProperty] private short _fontSize = 44;
+        [ObservableProperty] private string _fontName = "Source Han Serif VF";
+        [ObservableProperty] private short _fontSize = 40;
         [ObservableProperty] private string _fontWeight = nameof(Avalonia.Media.FontWeight.Bold);
         [ObservableProperty] private string _fontStyle = nameof(Avalonia.Media.FontStyle.Normal);
 
